bdfb89eda8d8b4720a3f225470535c3b7cadbab7
Yash Bavaskar <bavaskaryash1399@gmail.com>
2020-05-03 16:24:12 +0530

    Updates

---
+++ precache-manifest.89b360bfb1014d05af60664bec67838d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd121c0a0c916c2004a6b26c870a1f6e",
+    "url": "/covid-app/index.html"
+  },
+  {
+    "revision": "fba24b9b00f523293b51",
+    "url": "/covid-app/static/css/main.ff773b9a.chunk.css"
+  },
+  {
+    "revision": "a630b168eeab8d44dc12",
+    "url": "/covid-app/static/js/2.e2ff57ec.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-app/static/js/2.e2ff57ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fba24b9b00f523293b51",
+    "url": "/covid-app/static/js/main.76233e31.chunk.js"
+  },
+  {
+    "revision": "c06a842554f967b36b07",
+    "url": "/covid-app/static/js/runtime-main.378b84f3.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-app/static/media/covid.d7265326.png"
+  }
+]);
